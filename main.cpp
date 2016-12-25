0c8e9a5f6c3fbe3bf29a1db40ee74de348ab73dc
Laszlo Boros <iamsemmu@gmail.com>
2016-12-25 19:34:57 +0100

    main.cpp huge cleanup

--- main.cpp
+++ main.cpp
@@ -59,53 +59,6 @@ void DIE()
 }
 
 
-SU::Model cube, house;
-
-class Floating
-{
-public:
-	static std::list<Floating*> all;
-
-	SU::Object obj;
-
-	SU::Vector direction, destination;
-
-
-	Floating(bool distort = false)
-	{
-		direction = SU::Vector(randDouble(), randDouble(), randDouble()).getNormalized();
-
-		newDestination();
-		obj.position = destination;
-		newDestination();
-
-		obj.model = &house;
-
-		obj.transforming = distort;
-		obj.X = SU::Vector(1, randDouble(1) - 0.5, randDouble(1) - 0.5).getNormalized();
-		obj.Y = SU::Vector(randDouble(1) - 0.5, 1, randDouble(1) - 0.5).getNormalized();
-		obj.Z = SU::Vector(randDouble(1) - 0.5, randDouble(1) - 0.5, 1).getNormalized();
-
-		all.push_back(this);
-	}
-
-	void newDestination()
-	{
-		const double radius = 20;
-		destination = SU::Vector(randDouble(2.0) - 1.0, randDouble(2.0) - 1.0, randDouble(2.0) - 1.0).getNormalized() * radius;
-	}
-
-	void move()
-	{
-		direction = direction + (destination - obj.position).getNormalized();
-
-		obj.position += direction.getNormalized() / 20;
-
-		if ((destination - obj.position).getLength() < 1)
-			newDestination();
-	}
-};
-std::list<Floating*> Floating::all;
 
 #if USING_SDL1
 std::list<int> pressed_down_keys;
@@ -114,49 +67,8 @@ std::list<SDL_Keycode> pressed_down_keys;
 #endif
 
 
-/*
-	ORIGO KORULI FORGASHOZ
- */
-
-#define AUTO_ROTATION_AMOUNT 0.002
-
-bool cameraAutoRotating = true;
-double autoRotationAmount = AUTO_ROTATION_AMOUNT;
-double distance = 3;
-double heightAngle = 0;
-double rotationAngle = 0;
-
-
-
-
-
-
 int main( int argc, char* args[] )
 {
-/*	std::ofstream of;
-	of.open("randomvectors");
-	#define NUM 3
-	SU::Vector vs[NUM];
-	for(int i = 0; i < NUM; i++)
-	{
-		vs[i] = SU::Vector(randDouble(), randDouble(), randDouble());
-		vs[i].save(of);
-	}
-
-	of.close();
-
-	std::ifstream iff;
-	iff.open("randomvectors");
-	SU::Vector ivs[NUM];
-	for(int i = 0; i < NUM; i++)
-		ivs[i] = SU::Vector::load(iff);
-
-	for(int i = 0; i < NUM; i++)
-	{
-		std::cout << vs[i] << std::endl << ivs[i] << std::endl;
-	}*/
-
-
 	Uint32 currentSec = 0;
 	Uint32 currentSecFPS = 0;
 	Uint8 previousSecFPS = 0;
@@ -211,322 +123,6 @@ int main( int argc, char* args[] )
 
 
 
-
-
-
-	cube.add(new SU::Segment(0, -0.5, 0,	0.5, 0, 0));
-	cube.add(new SU::Segment(0, -0.5, 0,	-0.5, 0, 0));
-	cube.add(new SU::Segment(0, -0.5, 0,	0, 0, 0.5));
-	cube.add(new SU::Segment(0, -0.5, 0,	0, 0, -0.5));
-
-	cube.add(new SU::Segment(-0.5, 0, 0,	0, 0, -0.5));
-	cube.add(new SU::Segment(-0.5, 0, 0,	0, 0, +0.5));
-	cube.add(new SU::Segment(0.5, 0, 0,		0, 0, -0.5));
-	cube.add(new SU::Segment(0.5, 0, 0,		0, 0, +0.5));
-
-	cube.add(new SU::Segment(0, 0.5, 0,		0.5, 0, 0));
-	cube.add(new SU::Segment(0, 0.5, 0,		-0.5, 0, 0));
-	cube.add(new SU::Segment(0, 0.5, 0,		0, 0, 0.5));
-	cube.add(new SU::Segment(0, 0.5, 0,		0, 0, -0.5));
-
-
-
-
-
-	// front
-	house.add(new SU::Triangle(0, 0, 0,		1, 0, 0,	0, 1, 0,	SU::mapColor(200, 200, 50), true));
-	house.add(new SU::Triangle(1, 1, 0,		0, 1, 0,	1, 0, 0,	SU::mapColor(200, 200, 50), true));
-	house.add(new SU::Triangle(0, 1, 0,		1, 1, 0,	0.5, 1.5, 0.5,	SU::mapColor(150, 20, 20), true));
-
-	// right
-	house.add(new SU::Triangle(1, 0, 0,		1, 1, 1,	1, 1, 0,	SU::mapColor(200, 200, 50), true));
-	house.add(new SU::Triangle(1, 0, 0,		1, 0, 1,	1, 1, 1,	SU::mapColor(200, 200, 50), true));
-	house.add(new SU::Triangle(1, 1, 0,		1, 1, 1,	0.5, 1.5, 0.5,	SU::mapColor(150, 20, 20), true));
-
-	// left
-	house.add(new SU::Triangle(0, 0, 0,		0, 1, 0,	0, 1, 1,	SU::mapColor(200, 200, 50), true));
-	house.add(new SU::Triangle(0, 0, 0,		0, 1, 1,	0, 0, 1,	SU::mapColor(200, 200, 50), true));
-	house.add(new SU::Triangle(0, 1, 1,		0, 1, 0,	0.5, 1.5, 0.5,	SU::mapColor(150, 20, 20), true));
-
-	// back
-	house.add(new SU::Triangle(0, 0, 1,		0, 1, 1,	1, 1, 1,	SU::mapColor(200, 200, 50), true));
-	house.add(new SU::Triangle(0, 0, 1,		1, 1, 1,	1, 0, 1,	SU::mapColor(200, 200, 50), true));
-	house.add(new SU::Triangle(1, 1, 1,		0, 1, 1,	0.5, 1.5, 0.5,	SU::mapColor(150, 20, 20), true));
-
-	// bottom
-	house.add(new SU::Triangle(0, 0, 0,		0, 0, 1,	1, 0, 0,	SU::mapColor(10, 20, 50)));
-	house.add(new SU::Triangle(1, 0, 1,		1, 0, 0,	0, 0, 1,	SU::mapColor(10, 20, 50)));
-
-
-
-
-
-//	SU::Object origo;
-//	origo.model = &cube;
-
-	Floating *f = NULL, *ff = NULL, *fff = NULL, *ffff = NULL, *fffff = NULL;
-
-	for (int i = 0; i < 0; i++)
-	{
-		f = new Floating(false);
-/*		for (int i = 0; i < 4; i++)
-		{
-			ff = new Floating(true);
-			ff->obj.X = SU::Vector(0.5, 0, 0);
-			ff->obj.Y = SU::Vector(0, 0.5, 0);
-			ff->obj.Z = SU::Vector(0, 0, 0.5);
-			f->obj.addChild(&(ff->obj));
-
-			for (int i = 0; i < 4; i++)
-			{
-				fff = new Floating(true);
-				fff->obj.X = SU::Vector(0.5, 0, 0);
-				fff->obj.Y = SU::Vector(0, 0.5, 0);
-				fff->obj.Z = SU::Vector(0, 0, 0.5);
-				ff->obj.addChild(&(fff->obj));
-
-				for (int i = 0; i < 4; i++)
-				{
-					ffff = new Floating(true);
-					ffff->obj.X = SU::Vector(0.5, 0, 0);
-					ffff->obj.Y = SU::Vector(0, 0.5, 0);
-					ffff->obj.Z = SU::Vector(0, 0, 0.5);
-					fff->obj.addChild(&(ffff->obj));
-
-/*					for (int i = 0; i < 3; i++)
-					{
-						fffff = new Floating(true);
-						fffff->obj.X = SU::Vector(0.5, 0, 0);
-						fffff->obj.Y = SU::Vector(0, 0.5, 0);
-						fffff->obj.Z = SU::Vector(0, 0, 0.5);
-						ffff->obj.addChild(&(fffff->obj));
-					}*/
-//				}
-//			}
-//		}
-	}
-	bool move = false;
-
-	SU::Camera::position.z = -30;
-
-
-
-
-
-
-	// SHIP HERE
-
-	SU::Model hajtomu;
-	const int reszletesseg = 6;
-	for (int i = 0; i < reszletesseg; i++)
-	{
-		SU::Vector a(0, 1, 0), b(-2, 0.5, 0), x(1, 0, 0);
-		hajtomu.add(new SU::Quad(b.rotated(x, M_PI / reszletesseg * 2 * (i)),
-								 a.rotated(x, M_PI / reszletesseg * 2 * (i)),
-								 a.rotated(x, M_PI / reszletesseg * 2 * (i + 1)),
-								 b.rotated(x, M_PI / reszletesseg * 2 * (i + 1)), SU::mapColor(30, 30, 30), true));
-
-		SU::Vector aa(-2, 0.5, 0), bb(-1.9, 0.2, 0);
-		hajtomu.add(new SU::Quad(bb.rotated(x, M_PI / reszletesseg * 2 * (i)),
-								 aa.rotated(x, M_PI / reszletesseg * 2 * (i)),
-								 aa.rotated(x, M_PI / reszletesseg * 2 * (i + 1)),
-								 bb.rotated(x, M_PI / reszletesseg * 2 * (i + 1)), SU::mapColor(250, 50, 50), false));
-
-		SU::Vector aaa(-1.9, 0.2, 0), bbb(-3, 0, 0);
-		hajtomu.add(new SU::Quad(bbb.rotated(x, M_PI / reszletesseg * 2 * (i)),
-								 aaa.rotated(x, M_PI / reszletesseg * 2 * (i)),
-								 aaa.rotated(x, M_PI / reszletesseg * 2 * (i + 1)),
-								 bbb.rotated(x, M_PI / reszletesseg * 2 * (i + 1)), SU::mapColor(250, 250, 50), false));
-
-		SU::Vector c(0, 1, 0), d(1, 0.8, 0);
-		hajtomu.add(new SU::Quad(c.rotated(x, M_PI / reszletesseg * 2 * (i)),
-								 d.rotated(x, M_PI / reszletesseg * 2 * (i)),
-								 d.rotated(x, M_PI / reszletesseg * 2 * (i + 1)),
-								 c.rotated(x, M_PI / reszletesseg * 2 * (i + 1)), SU::mapColor(100, 100, 100), true));
-
-		SU::Vector cc(1, 0.8, 0), dd(1.1, 0.7, 0);
-		hajtomu.add(new SU::Quad(cc.rotated(x, M_PI / reszletesseg * 2 * (i)),
-								 dd.rotated(x, M_PI / reszletesseg * 2 * (i)),
-								 dd.rotated(x, M_PI / reszletesseg * 2 * (i + 1)),
-								 cc.rotated(x, M_PI / reszletesseg * 2 * (i + 1)), SU::mapColor(50, 100, 220), false));
-
-		SU::Vector ccc(1.1, 0.7, 0), ddd(0.5, 0, 0);
-		hajtomu.add(new SU::Quad(ccc.rotated(x, M_PI / reszletesseg * 2 * (i)),
-								 ddd.rotated(x, M_PI / reszletesseg * 2 * (i)),
-								 ddd.rotated(x, M_PI / reszletesseg * 2 * (i + 1)),
-								 ccc.rotated(x, M_PI / reszletesseg * 2 * (i + 1)), SU::mapColor(30, 30, 30), true));
-	}
-
-	SU::Model torzs;
-	torzs.add(new SU::Quad(3, 0, 0,		3, 0, 1,		0, 1, 1,		0, 1, 0, SU::mapColor(100, 100, 100), true));
-	torzs.add(new SU::Quad(3, 0, 0,		3.2, -0.5, 0,	3.2, -0.5, 1, 	3, 0, 1, SU::mapColor(100, 100, 100), true));
-	torzs.add(new SU::Quad(3, 0, 1,		3.2, -0.5, 1,	0, -0.5, 2,		0, 0, 2, SU::mapColor(30, 30, 30), true));
-	torzs.add(new SU::Triangle(3, 0, 1,		0, 0, 2,		0, 1, 1, SU::mapColor(30, 30, 30), true));
-
-
-	SU::Object torzsObject;
-	torzsObject.model = &torzs;
-	torzsObject.transforming = true;
-
-	SU::Object torzsTukor;
-	torzsTukor.model = &torzs;
-	torzsTukor.transforming = true;
-	torzsTukor.flipTriangles = true;
-	torzsTukor.Z.z = -1;
-
-	SU::Object torzsHatul;
-	torzsHatul.model = &torzs;
-	torzsHatul.transforming = true;
-	torzsHatul.flipTriangles = true;
-	torzsHatul.X.x = -2;
-
-	SU::Object torzsHatulT;
-	torzsHatulT.model = &torzs;
-	torzsHatulT.transforming = true;
-	torzsHatulT.Z.z = -1;
-	torzsHatulT.X.x = -2;
-
-	SU::Object torzslent;
-	torzslent.model = &torzs;
-	torzslent.transforming = true;
-	torzslent.flipTriangles = true;
-	torzslent.Y.y = -0.3;
-	torzslent.position.y = -0.644444;
-
-	SU::Object tlenttukor;
-	tlenttukor.model = &torzs;
-	tlenttukor.transforming = true;
-	tlenttukor.Y.y = -0.3;
-	tlenttukor.Z.z = -1;
-	tlenttukor.position.y = -0.644444;
-
-	SU::Object tlenthatul;
-	tlenthatul.model = &torzs;
-	tlenthatul.transforming = true;
-	tlenthatul.Y.y = -0.3;
-	tlenthatul.X.x = -2;
-	tlenthatul.position.y = -0.644444;
-
-	SU::Object tlehatu;
-	tlehatu.model = &torzs;
-	tlehatu.transforming = true;
-	tlehatu.Y.y = -0.3;
-	tlehatu.X.x = -2;
-	tlehatu.Z.z = -1;
-	tlehatu.flipTriangles = true;
-	tlehatu.position.y = -0.644444;
-
-
-
-
-	SU::Object ship, h1, h2, h3, h4;
-
-
-	// BUG! FIXME! gyermek object nem forog, csak ha o is transforming. pedig ettol nem kene fuggnie
-	h1.model = &hajtomu;
-	h1.transforming = true;
-	h1.position = SU::Vector(0, 3, 4);
-	h2.model = &hajtomu;
-	h2.transforming = true;
-	h2.position = SU::Vector(0, 3, -4);
-	h3.model = &hajtomu;
-	h3.transforming = true;
-	h3.position = SU::Vector(0, -3, -4);
-	h4.model = &hajtomu;
-	h4.transforming = true;
-	h4.position = SU::Vector(0, -3, 4);
-
-	SU::Model vaz;
-	vaz.add(new SU::Quad(-1, -1, -1,	1, -1, -1,		1, 1, -1,		-1, 1, -1,	SU::mapColor(30, 30, 30), true));
-	vaz.add(new SU::Quad(-1, -1, 1,		-1, 1, 1,		1, 1, 1,		1, -1, 1,	SU::mapColor(30, 30, 30), true));
-	vaz.add(new SU::Quad(-1, -1, 1,		-1, -1, -1,		-1, 1, -1,		-1, 1, 1,	SU::mapColor(100, 100, 100), true));
-	vaz.add(new SU::Quad(1, -1, -1,		1, -1, 1,		1, 1, 1,		1, 1, -1,	SU::mapColor(100, 100, 100), true));
-
-	SU::Object vaz1;
-	vaz1.model = &vaz;
-	vaz1.transforming = true;
-	vaz1.position = SU::Vector(0, 1.3, 2.6);
-	vaz1.Y.y = 6;
-	vaz1.scale(0.21);
-	vaz1.rotateAroundX(1.4);
-
-	SU::Object vaz2;
-	vaz2.model = &vaz;
-	vaz2.transforming = true;
-	vaz2.position = SU::Vector(0, 1.3, -2.6);
-	vaz2.Y.y = 6;
-	vaz2.scale(0.21);
-	vaz2.rotateAroundX(-1.4);
-
-	SU::Object vaz3;
-	vaz3.model = &vaz;
-	vaz3.transforming = true;
-	vaz3.position = SU::Vector(0, -1.45, -2.65);
-	vaz3.Y.y = 5;
-	vaz3.scale(0.21);
-	vaz3.rotateAroundX(1.4);
-
-	SU::Object vaz4;
-	vaz4.model = &vaz;
-	vaz4.transforming = true;
-	vaz4.position = SU::Vector(0, -1.45, 2.65);
-	vaz4.Y.y = 5;
-	vaz4.scale(0.21);
-	vaz4.rotateAroundX(-1.4);
-
-
-
-	ship.addChild(&h1);
-	ship.addChild(&h2);
-	ship.addChild(&h3);
-	ship.addChild(&h4);
-	ship.addChild(&torzsObject);
-	ship.addChild(&torzsTukor);
-	ship.addChild(&torzsHatul);
-	ship.addChild(&torzsHatulT);
-	ship.addChild(&torzslent);
-	ship.addChild(&tlenttukor);
-	ship.addChild(&tlenthatul);
-	ship.addChild(&tlehatu);
-	ship.addChild(&vaz1);
-	ship.addChild(&vaz2);
-	ship.addChild(&vaz3);
-	ship.addChild(&vaz4);
-
-	ship.transforming = true;
-	ship.rotateAroundY(M_PI / 2);
-
-	class UnShader : public SU::Shaders::Shader
-	{
-		void shadeTriangle(SU::Triangle* t)
-		{
-			t->lighted = false;
-		}
-	};
-
-	class JiggleShader : public SU::Shaders::Shader
-	{
-		void shadeTriangle(SU::Triangle* t)
-		{
-			t->P1 += SU::Vector(randDouble(0.1), randDouble(0.1), randDouble(0.1));
-			t->P2 += SU::Vector(randDouble(0.1), randDouble(0.1), randDouble(0.1));
-			t->P3 += SU::Vector(randDouble(0.1), randDouble(0.1), randDouble(0.1));
-		}
-	};
-
-	class RainbowShader : public SU::Shaders::Shader
-	{
-		void shadeTriangle(SU::Triangle* t)
-		{
-			t->color = SU::mapColor(128 + rand() % 128, 128 + rand() % 128, 128 + rand() % 128);
-		}
-	};
-
-	SU::Shaders::shaders.push_back(new JiggleShader);
-
-
-
-
 	while (running)
 	{
 		if (SDL_GetTicks() / 1000 != currentSec)
@@ -565,18 +161,11 @@ int main( int argc, char* args[] )
 						switch (e.key.keysym.sym)
 						{
 							case SDLK_ESCAPE:
-								running = false;
-							break;
-
 							case SDLK_q:
 								running = false;
 							break;
 
-							case SDLK_SPACE:
-								cameraAutoRotating = !cameraAutoRotating;
-							break;
-
-							case SDLK_0:
+							case SDLK_F12:
 								debug_text_visible = !debug_text_visible;
 							break;
 
@@ -602,52 +191,6 @@ int main( int argc, char* args[] )
 
 							case SDLK_F6:
 								SU::toggleFlag(SU::Flags::LIGHTING);
-							break;
-
-							case SDLK_F7:
-							break;
-
-							case SDLK_LEFT:
-							{
-								if (cameraAutoRotating)
-								{
-									autoRotationAmount -= AUTO_ROTATION_AMOUNT;
-								}
-								else
-								{
-									pressed_down_keys.push_back(SDLK_LEFT);
-								}
-							}
-							break;
-
-							case SDLK_RIGHT:
-							{
-								if (cameraAutoRotating)
-								{
-									autoRotationAmount += AUTO_ROTATION_AMOUNT;
-								}
-								else
-								{
-									pressed_down_keys.push_back(SDLK_RIGHT);
-								}
-							}
-							break;
-
-							case SDLK_RETURN:
-							{
-								if (cameraAutoRotating)
-								{
-									SU::Camera::position = SU::Vector(0, 0, -30);
-									SU::Camera::lookDirection = SU::Vector(0, 0, 1);
-									SU::Camera::upDirection = SU::Vector(0, 1, 0);
-
-									cameraAutoRotating = false;
-								}
-								else
-								{
-									cameraAutoRotating = true;
-								}
-							}
 							break;
 
 							default:
@@ -694,127 +237,11 @@ int main( int argc, char* args[] )
 		{
 			switch(k)
 			{
-				case SDLK_LEFT:
-				{
-					SU::Camera::position -= SU::Camera::rightDirection.getNormalized() * CAMERA_MOVEMENT_AMOUNT;
-				}
-				break;
-
-				case SDLK_RIGHT:
-				{
-					SU::Camera::position += SU::Camera::rightDirection.getNormalized() * CAMERA_MOVEMENT_AMOUNT;
-				}
-				break;
-
-				case SDLK_DOWN:
-				{
-					if (cameraAutoRotating)
-					{
-						distance += 0.01;
-					}
-					else
-					{
-						SU::Camera::position -= SU::Camera::lookDirection.getNormalized() * CAMERA_MOVEMENT_AMOUNT;
-					}
-				}
-				break;
-
-				case SDLK_UP:
-				{
-					if (cameraAutoRotating)
-					{
-						distance -= 0.01;
-					}
-					else
-					{
-						SU::Camera::position += SU::Camera::lookDirection.getNormalized() * CAMERA_MOVEMENT_AMOUNT;
-					}
-				}
-				break;
-
-				case SDLK_RSHIFT:
-				{
-					if (cameraAutoRotating)
-					{
-						heightAngle -= 0.01;
-					}
-					else
-					{
-						SU::Camera::position += SU::Camera::upDirection.getNormalized() * CAMERA_MOVEMENT_AMOUNT;
-					}
-				}
-				break;
-
-				#if USING_SDL1
-				case SDLK_KP1:
-				#else
-				case SDLK_KP_1:
-				#endif
-				{
-					if (cameraAutoRotating)
-					{
-						heightAngle += 0.01;
-					}
-					else
-					{
-						SU::Camera::position -= SU::Camera::upDirection.getNormalized() * CAMERA_MOVEMENT_AMOUNT;
-					}
-				}
-				break;
-
-				case SDLK_KP_MINUS:
-				{
-					SU::Camera::FOV--;
-				}
-				break;
-
-				case SDLK_KP_PLUS:
-				{
-					SU::Camera::FOV++;
-				}
-				break;
-
-				case SDLK_w:
-					SU::Camera::pitch(CAMERA_ROTATION_AMOUNT);
-				break;
-
-				case SDLK_s:
-					SU::Camera::pitch(-CAMERA_ROTATION_AMOUNT);
-				break;
-
-				case SDLK_a:
-					SU::Camera::yaw(-CAMERA_ROTATION_AMOUNT);
-				break;
-
-				case SDLK_d:
-					SU::Camera::yaw(CAMERA_ROTATION_AMOUNT);
-				break;
-
-				case SDLK_q:
-					SU::Camera::roll(CAMERA_ROTATION_AMOUNT);
-				break;
-
-				case SDLK_e:
-					SU::Camera::roll(-CAMERA_ROTATION_AMOUNT);
-				break;
-
 				default: break;
 			}
 		}
 
-		if (cameraAutoRotating)
-		{
-			SU::Camera::position = SU::Vector(0, 0, -10 * distance).rotated(SU::Vector(1, 0, 0), heightAngle).rotated(SU::Vector(0, 1, 0), rotationAngle);
-			SU::Camera::upDirection = SU::Vector(0, 1, 0).rotated(SU::Vector(1, 0, 0), heightAngle).rotated(SU::Vector(0, 1, 0), rotationAngle);
-			SU::Camera::lookDirection = -SU::Camera::position;
-			rotationAngle += autoRotationAmount;
-		}
-
 		SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0, 0, 0));
-
-		if (move)
-			for (Floating* f : Floating::all)
-				f->move();
 
 		SU::render();
 		currentSecFPS++;
@@ -872,16 +299,12 @@ int main( int argc, char* args[] )
 		#if USING_SDL1
 			SDL_Flip(surface);
 		#else
-			// TODO:
-			// which is faster? this method or SDL_CreateSoftwareRenderer?
-			// or CreateTextureFromSurface() ???
 			SDL_UpdateTexture(texture, NULL, surface->pixels, surface->pitch);
-
 			SDL_RenderCopy(renderer, texture, NULL, NULL);
 			SDL_RenderPresent(renderer);
 		#endif
 
-		//SDL_Delay(1);
+		SDL_Delay(1);
 	}
 
 	return 0;
